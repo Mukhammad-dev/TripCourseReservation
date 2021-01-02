c4cb1db18ca0c55a5b477413fa850b2c1a3b4f2a
Shakhzod Kholikov <imshakhzod@gmail.com>
2021-01-02 23:01:10 +0100

    added new windows for the course and trip

---
+++ UpdateCourseWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TripCourseReservation
+{
+    /// <summary>
+    /// Interaction logic for UpdateCourseWindow.xaml
+    /// </summary>
+    public partial class UpdateCourseWindow : Window
+    {
+        public UpdateCourseWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
